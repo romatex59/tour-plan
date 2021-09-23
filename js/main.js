8e0cc72f9bd9b60182b090aad1bc92022ef3757d
romatex59 <69964988+romatex59@users.noreply.github.com>
2021-09-23 10:44:56 +0500

    create: slider

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  effect: "coverflow",
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
