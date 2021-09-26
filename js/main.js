e4616deba7c04f0173f230b0fcc60c391b004c31
romatex59 <romatexdev@gmail.com>
2021-09-26 11:32:55 +0500

    create: reviews section html,css,media

--- js/main.js
+++ js/main.js
@@ -14,11 +14,33 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.slider-button--prev',
   },
 
-  effect: "coverflow",
+
 
   keyboard: {
     enabled: true,
     onlyInViewport: false,
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const reswiper = new Swiper('.reviews-swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  //loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-button--next',
+    prevEl: '.reviews-button--prev',
   },
 
   // And if we need scrollbar
